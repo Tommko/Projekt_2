899a6c6704a95a17496c2cfd6678725f1abc6922
Tommko <kolarik-tomas@seznam.cz>
2025-08-18 21:47:30 +0200

    update_2

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ length_of_number = 4
 bulls = 0
 cows = 0
 counter = 1
+player_number = ""
 
 introduction = f"""
 Hi there!
@@ -23,98 +24,55 @@ Let's play a bulls and cows game.
 
 def generate_number():                                             #generate random number
     digit_choices = range(0,10,1)
-    numbers = random.sample(digit_choices,4)
+    numbers = random.sample(digit_choices,length_of_number)
     generated_number = ""
     if numbers[0] == 0:
-        numbers = random.sample(digit_choices,4)
+        numbers = random.sample(digit_choices,length_of_number)
     
     for X in numbers:
         generated_number = generated_number + str(X)
     return generated_number
 
-def check_uniqueness(entry):
-    
-    for A in entry:
-        if str(entry).count(A) > 1:
-            uniqueness_help.append(False)
+def number_check(entry):                                           #checking if the input is right
+    for number in entry:
+        if str(entry).count(number) > 1 or entry.isnumeric() != True or str(entry[0]) == "0"  or len(entry) != length_of_number:
+            check = False
         else:
-            uniqueness_help.append(True)
-    
-    return uniqueness_help
-
-def player_number_check(variable_1):                               #checks if digits in players number are unique
-    global uniqueness_help
-
-    uniqueness_help = []
-
-    check_uniqueness(variable_1)  
-
-    if uniqueness_help.count(True) < length_of_number:
-        unique_number_output = False
-    else:
-        unique_number_output = True
-        
-    X = [len(variable_1) == length_of_number, variable_1.isnumeric(), str(variable_1[0]) != "0", unique_number_output]
-
-    while X.count(True) != length_of_number:
-        
-        uniqueness_help = []
-
-        variable_1 = input("Warning! Number must be length_of_number digits long, can't contain duplicates, letters or start with 0!: ")
-        
-        check_uniqueness(variable_1)
-
-        if uniqueness_help.count(True) < length_of_number:
-            unique_number_output = False
+            check = True
+            
+        if check == True:
+            decision = entry
+            pass
         else:
-            unique_number_output = True
-        X = [len(variable_1) == length_of_number, variable_1.isnumeric(), str(variable_1[0]) != "0", unique_number_output]
-
-    return variable_1
+            decision = print("Warning! Number must be length_of_number digits long, can't contain duplicates, letters or start with 0!")
+            break
+    return decision
                                  
-def bulls_numnber(bulls_input):                                    #decision about singular or plural of bulls
-    if bulls_input > 1:
-        bulls_count = "bulls"
+def plural(word,value):                                            #creates plural
+    if value == 1:
+        pass
     else:
-        bulls_count = "bull"
-    return bulls_count
+        if word[-1] == "s":
+            word = word + "es"
+        else:
+            word = word + "s"
+    return word
 
-def cows_number(cows_input):                                       #decision about singular or plural of cows
-    if cows_input > 1:
-        cows_count = "cows"
+def main_function(bulls, cows, counter):                           #main function of the game
+    if player_number != computer_number:
+        for index, digit in enumerate(player_number, 0):
+            if digit == computer_number[index]:
+                bulls = bulls + 1
+                
+            elif digit in computer_number:
+                cows = cows + 1
+                    
+        print(bulls, plural("bull",bulls), "," , cows, plural("cow",cows))
+        print(lines)
+            
     else:
-        cows_count = "cow"
-    return cows_count
-
-def guesses_number(guesses_input):                                 #decision about singular or plural of guesses
-    if guesses_input > 1:
-        guesses_count = "gueses!"
-    else:
-        guesses_count = "guess!"
-    return guesses_count
-
-def main_function():                                               #main function of the game
-    global player_number
-    global bulls
-    global cows
-    global counter
-    for i, A in enumerate(player_number, 0):
-        if A == computer_number[i]:
-            bulls = bulls + 1
-        elif A in computer_number:
-            cows = cows + 1
-        
-    print(bulls, bulls_numnber(bulls), "," , cows, cows_number(cows))
-    print(lines)
+        print("You've won! You've made it in", counter ,plural("guess",counter))
     
-    
-    if bulls < length_of_number:
-        variable_1 = input("Insert number please: ")
-
-        player_number = str(player_number_check(variable_1))
-        counter = counter + 1
-    
-    return bulls, cows, counter
 
 if __name__ == "__main__":
 
@@ -122,17 +80,13 @@ if __name__ == "__main__":
 
     print(introduction)
 
-    variable_1 = input("Insert number please: ")         
+    while player_number != computer_number:
+        variable_1 = input("Insert number please: ")         
 
-    player_number = str(player_number_check(variable_1))  
-
-
-    while bulls < length_of_number:
-        
+        player_number = str(number_check(variable_1))  
+            
         bulls = 0
         cows = 0
-        
-        main_function()
-        
-    else:
-        print("You've won! You've made it in", counter ,guesses_number(counter))
+            
+        main_function(bulls,cows,counter)
+        counter = counter + 1
